caa1beb829d1af4b8c78367441431a394b9cfcc9
abehidek <hidek.abe@outlook.com>
2022-07-26 11:19:08 -0300

    Add some testing functionalities of elixir lang

--- getting_started.ex
+++ getting_started.ex
@@ -1,1 +1,21 @@
 IO.puts("Hello World")
+
+sumTwoValues = fn a, b -> a + b end
+
+IO.puts(sumTwoValues.(1, 3))
+
+:atom
+
+variable =
+  case {4, 3, 2, 1} do
+    {1, 2, _, 4} ->
+      "nice"
+
+    {2, 2, 2, 2} ->
+      "nooo"
+
+    _ ->
+      "sad"
+  end
+
+IO.puts(variable)
