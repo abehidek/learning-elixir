f98ffee9b03a1597d1f6b8506ae7eb738ffffefa
abehidek <hidek.abe@outlook.com>
2022-07-28 10:33:36 -0300

    Add Enum content

--- exercises/lib/exercises.ex
+++ exercises/lib/exercises.ex
@@ -1,4 +1,4 @@
-defmodule Projects do
+defmodule Exercises do
   @moduledoc """
   Documentation for `Projects`.
   """
@@ -14,5 +14,21 @@ defmodule Projects do
   """
   def hello do
     :world
+  end
+
+  def sum(a, b) do
+    IO.puts("A: #{a}, B: #{b}")
+    a + b
+  end
+
+  def multiply(a, b) do
+    a * b
+  end
+
+  def pipe do
+    foo = 15
+    bar = foo |> sum(5) |> multiply(30)
+
+    IO.puts("Result: #{bar}")
   end
 end
