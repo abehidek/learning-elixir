dda2e338334faa8183d0a45ff4010fa5eeb9ac1b
abehidek <hidek.abe@outlook.com>
2023-10-12 17:56:04 -0300

    course(octualium): improve mode fn with validators

--- octallium/stats/lib/central_tendency/mode.ex
+++ octallium/stats/lib/central_tendency/mode.ex
@@ -1,15 +1,28 @@
 defmodule Stats.CentralTendency.Mode do
   @moduledoc false
 
-  def mode(nums) when is_list(nums) do
-    ranked_map = nums |> Enum.frequencies()
+  @spec mode([number()]) :: [number()] | {atom(), String.t()}
+  def mode([]), do: Stats.Errors.invalid_data_type()
 
-    ranked_map
-    |> Map.values()
-    |> Enum.max()
-    |> mode_func(ranked_map)
+  def mode(nums) when is_list(nums) do
+    case Stats.Validators.validate_num_list(nums) do
+      {true, _} ->
+        ranked_map = nums |> Enum.frequencies()
+
+        ranked_map
+        |> Map.values()
+        |> Enum.max()
+        |> mode_func(ranked_map)
+
+      {false, _} ->
+        IO.inspect(nums, label: "ERROR - Invalid data type of list")
+        Stats.Errors.invalid_data_type()
+    end
   end
 
+  def mode(_), do: Stats.Errors.invalid_data_type()
+
+  @spec mode_func(number(), %{number() => number()}) :: [number()]
   defp mode_func(1, _ranked_map), do: nil
 
   defp mode_func(max, ranked_map) do
